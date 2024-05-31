e296b4bf4db20d3ffd1cef4c642aa036b4d28254
Talaini <10530576+talainimohammed@users.noreply.github.com>
2024-05-31 11:36:01 +0100

    flip animation

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -7,7 +7,18 @@ const Product = (props) => {
     return (
         <div className={props.className}>
             <h1>AGROSOL-K12</h1>
-            <img className='productlogo' src={Produitlogo} alt="produitlogo"/>
+            <div className='flip-card'>
+                <div className='flip-card-inner'>
+                    <div className='flip-card-front'>
+                        <img className='productlogo' src={Produitlogo} alt="produitlogo"/>
+                    </div>
+                    <div className='flip-card-back'>
+                        <h1>Product Information</h1> 
+                        <p>AGROSOL-K12</p>
+                        <p>AGROSOL-K12</p>
+                    </div>
+                </div>
+            </div>
             <p className='price'>Prix:<span>000MAD</span></p>
             <p className='review'><img src={Star} alt="star" height={30} /><span>4.9</span><a><img src={Addtocart} alt="addtocart" height={40}/></a></p>
         </div>
